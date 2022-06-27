e78a84f2375ef631818c393b5341dd2c80c0e302
Iagocanalejas <iagocanalejas@gmail.com>
2022-06-27 10:04:10 +0200

    fix: flatpak packages not working with fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -44,6 +44,16 @@ complete -xc spark -n __fish_use_subcommand -a --version -d "$spark_version"
 complete -xc spark -n __fish_use_subcommand -a --min -d "Minimum range value"
 complete -xc spark -n __fish_use_subcommand -a --max -d "Maximum range value"
 
+### FLATPAK ###
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
+
 function spark -d "sparkline generator"
     if isatty
         switch "$argv"
